cf20a69c8656905b7c7c08e23260924c5de49e94
jccd-dev <johncarlo.digay@outlook.com>
2025-02-14 14:07:28 +0800

    fix: Types of property 'params' are incompatible.

--- src/app/my-message-for/[name]/page.tsx
+++ src/app/my-message-for/[name]/page.tsx
@@ -3,12 +3,12 @@ import LoadingMessage from '@/components/custom/loadingMessage';
 import { Suspense } from 'react';
 import MessageContent from './message-content';
 
-interface MyMessageProps {
-    params: { name: Promise<string> };
-}
-
-export default async function MyMessage({ params }: MyMessageProps) {
-    const name = await params.name;
+export default async function MyMessage({
+    params,
+}: {
+    params: Promise<{ name: string }>;
+}) {
+    const name = (await params).name;
     return (
         <main className="min-h-screen flex flex-col items-center justify-center bg-pink-50">
             <HeartsBackground />
